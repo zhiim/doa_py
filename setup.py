be4502f9e79aba8f42c87c4bd91e91813e2df35f
Q Xu <xuq3196@outlook.com>
2024-01-18 12:36:25 +0800

    change project structure, build it as a python package

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='<your-package-name>',
+    version='0.1',
+    packages=find_packages(),
+    description='<your-package-description>',
+    author='<your-name>',
+    author_email='<your-email>',
+    url='<your-package-url>',
+    install_requires=[
+        # list of dependencies
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
